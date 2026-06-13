09ee36fbe1a2bfa2e40e85f25aa0d1410de6a168
kevin-ariel-belgara <kevinariel.belgara@estudiantes.unahur.edu.ar>
2026-06-13 14:35:16 -0300

    Estableciedo el setup para empezar

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('Conexión a MongoDB establecida con éxito.');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
